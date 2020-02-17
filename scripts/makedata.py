668b3af80323e831349fb792d5bd17409ec15e7b
thomasms <stainer.tom@gmail.com>
2020-02-17 21:35:02 +0000

    Added halflives to JSON database

--- scripts/makedata.py
+++ scripts/makedata.py
@@ -8,12 +8,64 @@ import pypact as pp
 filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),
     '..', 'reference', 'printlib5_decay_2012.out')
 
+# full dataset 
 data = collections.defaultdict()
 
+# get halflives 
+halflifedata = collections.defaultdict()
+
+# use FISPACT-II API instead if available
+try:
+    import sys
+    import pyfispact as pf
+
+    nd_path = os.getenv('NUCLEAR_DATA')
+
+    log = pf.Monitor()
+    pf.initialise(log)
+
+    # we only care about errors
+    log.setlevel(pf.severity.error)
+
+    ndr = pf.io.NuclearDataReader(log)
+    nd = pf.NuclearData(log)
+
+    # just load decay data
+    ndr.setpath(pf.io.ND_IND_NUC_KEY(), os.path.join(nd_path, 'decay', 'decay_2012_index_2012'))
+    ndr.setpath(pf.io.ND_DK_ENDF_KEY(), os.path.join(nd_path, 'decay', 'decay_2012'))
+
+    # callback for nuclear data loader
+    def loadfunc(k, p, index, total):
+        print(" [{}/{}]  Reading {}: {}".format(index, total, k, p), end="\r")
+        sys.stdout.write("\033[K")
+
+    # load data
+    ndr.load(nd, op=loadfunc)
+
+    # check for error
+    if log.hasfatal():
+        print(log)
+        sys.exit(1)
+
+    # get half life
+    for i, z in enumerate(nd.getdecayzais()):
+        if not nd.getdecayisstable(i):
+            halflifedata[z] = nd.getdecayhalflife(i)
+
+except:
+    print("WARNING: Without the API this does not get the half-lives!")
+
+# TODO: if have pyfispact then do it the correct way
+# printlib5 does not have halflives - we need this too!
 with pp.PrintLib5Reader(filename) as output:
     for entry in output.spectral_data:
         if entry.type == "no spectral data":
             continue
+
+        # check if the halflife has been set
+        halflife = -1
+        if entry.zai in halflifedata:
+            halflife = halflifedata[entry.zai]
 
         # remove whitespace
         typename = "_".join(entry.type.split(" "))
@@ -23,6 +75,7 @@ with pp.PrintLib5Reader(filename) as output:
         if name not in data:
             data[name] = {
                 "zai": entry.zai,
+                "halflife": halflife,
                 typename: {}
             }
 
@@ -45,5 +98,6 @@ with pp.PrintLib5Reader(filename) as output:
 outputfile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
     '..', 'reference', 'lines_decay_2012.json')
 
+print("Creating JSON database...")
 with open(outputfile, 'wt') as flines:
     flines.write(json.dumps(data, indent=4, sort_keys=False))
