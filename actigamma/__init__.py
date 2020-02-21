6b3529e8d36da70fda677cb316737aca0c2a5b4a
thomasms <stainer.tom@gmail.com>
2020-02-21 15:23:24 +0000

    Updated version due to name change of API

--- actigamma/__init__.py
+++ actigamma/__init__.py
@@ -1,7 +1,7 @@
 # version in two places - here and .VERSION file
 # TODO: somehow unify this, without reading the .VERSION every time 
 # the package is imported
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .database import ReadOnlyDatabase, DatabaseJSONFileLoader
 from .decorators import *
