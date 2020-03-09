d304ce071718abf7b413b4d0bc10be3684e928c2
thomasms <stainer.tom@gmail.com>
2020-03-09 15:37:39 +0000

    Adding a chart of the nuclides plot in examples to show gamma lines

---
+++ examples/plotchartofnuclides.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import cm
+import numpy as np
+import pypact as pp
+
+import actigamma as ag
+
+
+def nuclide_mapping(Z, A):
+    """
+        Return X, Y position where A is on X
+        and Z is on Y.
+        
+        Note different to input!        
+    """
+    # split chart into four pieces 
+    if Z <= 24:
+        return int(A+15), int(Z+58)
+    elif Z <= 64:
+        return int(A-40), int(Z+8)
+    elif Z <= 94:
+        return int(A-125), int(Z-52)
+    else:
+        return int(A-178), int(Z-90)
+
+
+def make_full_nuclide_chart(data_matrix=None, figsize=(12, 8), cmap='Set1'):
+    XSIZE = 135
+    YSIZE = 90
+    # define an empty matrix of size (90,135)
+    # X and Y are inverted
+    if data_matrix is None:
+        data_matrix = np.zeros(shape=(YSIZE, XSIZE))
+        data_matrix[:, :] = 0.0
+        
+    assert data_matrix.shape == (YSIZE, XSIZE)
+
+    # make a chart of the nuclides
+    fig, ax = plt.subplots(figsize=figsize)
+    
+    # draw Z and N labels with arrows near H1
+    X, Y = nuclide_mapping(1, 1)
+    plt.text(X-4, Y, r'Z', fontsize=18)
+    plt.text(X, Y-4, r'N', fontsize=18)
+
+    # Z arrow
+    plt.arrow(X-3, Y+3, 0, 4, fc='k', ec='k', alpha=1.0, width=0.1,
+                head_width=1, head_length=None)
+
+    # N arrow
+    plt.arrow(X+3, Y-3, 4, 0, fc='k', ec='k', alpha=1.0, width=0.1,
+                head_width=1, head_length=None)
+
+    all_isotopes = []
+    mZ = -1
+    mA = -1
+    for d in pp.NUCLIDE_DICTIONARY:
+        for i in d['isotopes']:
+            mZ = max(mZ, d['Z'])
+            mA = max(mA, i)
+
+            X, Y = nuclide_mapping(d['Z'], i)
+            all_isotopes.append((X, Y))
+            rect = patches.Rectangle((X-0.5, Y-0.5), 1, 1, linewidth=1, edgecolor='k',facecolor='none')
+            ax.add_patch(rect)
+
+    # set everything that is not in the nuclide chart to grey
+    # for 'Set1' this is 8
+    for x in range(XSIZE):
+        for y in range(YSIZE):
+            if (x, y) not in all_isotopes:
+                data_matrix[y, x] = 8
+            
+    im = plt.imshow(data_matrix, cmap=cmap)
+    # ax.invert_yaxis()
+    ax.axis('off')
+    plt.xlabel("A", fontsize=16)
+    plt.ylabel("Z", fontsize=16)
+    plt.xlim([0, XSIZE])
+    plt.ylim([0, YSIZE])
+    plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.99, wspace=None, hspace=None)
+    # fig.colorbar(im, cax = fig.add_axes([0.91, 0.2, 0.03, 0.6]))
+    
+    return fig, ax
+
+
+"""
+    Main script 
+"""
+SPECTYPE = "gamma"
+# 'Set1', 'Set2', 'Dark2' or 'tab10' work well
+cmap = 'Set1'
+# setup the DB
+db = ag.Decay2012Database()
+allradionuclides = db.allnuclidesoftype(spectype=SPECTYPE)
+
+# for Set1 1 == blue - all nuclides that are either stable or no data in database
+value = 1
+allCount = pp.NUMBER_OF_ISOTOPES
+allnuclideArtist = plt.Line2D((0,1),(0,0), color=cm.__getattribute__(cmap)(value), marker='o', linestyle='')
+data_matrix = np.ones(shape=(135, 90))
+data_matrix[:, :] = value
+
+# show all radioisotopes
+# for Set1 0 == red - all nuclides that are unstable and exist in database
+value = 0
+unstablenuclideArtist = plt.Line2D((0,1),(0,0), color=cm.__getattribute__(cmap)(value), marker='o', linestyle='')
+for nuc in db.allnuclides:
+    z, a, _ = pp.library.nuclidelib.get_zai_props(db.getzai(nuc))
+    x, y = nuclide_mapping(z, a)
+    data_matrix[x, y] = value
+    
+# show all radioisotopes for that spectype
+# for Set1 2 == green - all nuclides that are unstable and have gamma lines in database
+value = 2
+allGammaCount = len(allradionuclides)
+allUnstableCount = len(db.allnuclides) - allGammaCount
+gammanuclideArtist = plt.Line2D((0,1),(0,0), color=cm.__getattribute__(cmap)(value), marker='o', linestyle='')
+for nuc in allradionuclides:
+    z, a, _ = pp.library.nuclidelib.get_zai_props(db.getzai(nuc))
+    x, y = nuclide_mapping(z, a)
+    data_matrix[x, y] = value
+
+fig1, ax1 = make_full_nuclide_chart(data_matrix=data_matrix.transpose(), cmap=cmap)
+ax1.legend([ allnuclideArtist, unstablenuclideArtist, gammanuclideArtist],
+           [ 'Either stable or not in database = {}'.format(allCount), 
+           'Unstable (no {} lines) = {}'.format(SPECTYPE, allUnstableCount), 
+           'Unstable (has {} lines) = {}'.format(SPECTYPE, allGammaCount)])
+plt.show()
